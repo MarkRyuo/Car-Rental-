186060c9f8a9a70745fac8d7bfb69a67e83d2628
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-09 22:09:18 +0800

    udpate herE

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def userPass() :
   if user_pass in user1["password"] :
     print("Welcome to Arc Car Rental")
   while not user_pass in user1["password"] :
-    user_pass = input("Enter your password again: ") 
+    print("Input your password")
+    user_pass = input("Try again: ") 
 
 userPass()  
